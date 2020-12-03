8131a88106f446eca0c8487919fdb6125a0d7129
Ubuntu <ubuntu@ip-172-31-43-17.us-east-2.compute.internal>
2020-12-03 20:48:33 +0000

    static page

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
 const http = require('http');
 const URL = require('url');
-let counter = 0;
+const path = require('path');
+const fs = require('fs');
+//let counter = 0;
 
 function handler(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
     const parsedURL = URL.parse(req.url);
     switch (parsedURL.pathname) {
         case "/":
-            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-            res.write("Main page<br>");
-            res.write("<a href='/counter'>Счетчик<br></a>");
-            res.write("<a href='/reset'>Сброс</a>");
-            serveIndex(req, res);
+            serveStatic(req, res, "index.html");
             break;
-        case "/counter":
-            serveCounter(req, res);
+        case "/static/main.css":
+            serveStatic(req, res, "main.css")
             break;
-        case "/reset":
-            serveReset(req, res);
-            break;
-        default:
-            serveNotFound(req, res);
+        case "/static/product.png":
+            serveStatic(req, res, "product.png");
             break;
     }
     console.log("Request, url:", req.url);
     res.end();
 }
 
-function serveIndex(req, res) {
+function serveStatic(req, res, customFileName) {
+    const filename = customFileName ? customFileName : path.basename(req.url);
+    const extension = path.extname(filename);
+    switch (extension) {
+        case '.html':
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' });
+            break;
+        case '.css':
+            res.writeHead(200, { 'Content-Type': 'text/css; charset=utf8' });
+            break;
+        case '.png':
+            res.writeHead(200, { 'Content-Type': 'image/png; charset=utf8' });
+            break;
+    }
+    const content = fs.readFileSync("static/" + filename);
+    res.write(content);
+}
+
+const server = http.createServer(handler);
+server.listen(process.env.PORT);
+
+
+// Not in use
+/*function serveIndex(req, res) {
     if (req.url != "/favicon.ico") {
         counter++;
     }
@@ -46,6 +63,4 @@ function serveNotFound(req, res) {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.write("Not found");
 }
-
-const server = http.createServer(handler);
-server.listen(process.env.PORT);
+*/
