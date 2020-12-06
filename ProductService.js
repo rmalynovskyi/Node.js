fec6fc9685a2014ffaf160dd2c54414c0bb24384
Ubuntu <ubuntu@ip-172-31-43-17.us-east-2.compute.internal>
2020-12-06 22:18:02 +0000

    product key and slug added

--- ProductService.js
+++ ProductService.js
@@ -2,23 +2,31 @@ let products = [];
 module.exports = {
   init() {
     products.push({
+      slug: "pvu-turkov-zenit-350-heco",
+      key: 111,
       title: "ПВУ Turkov ZENIT 350 HECO",
-      URL: "/product",
-      imgURL: "static/product.png",
+      imgURL: "/static/product.png",
       description: "Вентиляционная установка с рекуперацией тепла и влаги",
+      details: "Вентиляционная установка с рекуперацией тепла и влаги в легком и универсальном корпусе из вспенненного полипропилена предназначена для поддержания климата в жилых помещениях,или небольших офисах, магазинах.",
       price: 5000
     });
 
     products.push({
+      slug: "beats-studio-wireless-over-ear-headphones",
+      key: 222,
       title: "Beats Studio Wireless Over-Ear Headphones",
-      URL: "/product1",
-      imgURL: "static/product1.png",
+      imgURL: "/static/product1.png",
       description: "Беспроводные наушники Beats Studio",
+      details: "При изготовлении корпуса Beats Studio были использованы прецизионные формы и металлизация поверхности для высокой износостойкости.Наушники поддерживают протокол Bluetooth v4.0 и профили HFP, HSP, A2DP, AVRCP.",
       price: 10000
     });
   },
 
   getProducts() {
     return products;
+  },
+
+  getProductByKey(customKey) {
+    return products.find(product => product.key == customKey);
   }
 };
--- index.js
+++ index.js
@@ -8,18 +8,17 @@ const ProductService = require('./ProductService.js');
 function handler(req, res) {
     try {
         const parsedURL = URL.parse(req.url);
+
+        if (parsedURL.pathname.indexOf("/product/") === 0) {
+            serveProduct(req, res, "product.html");
+        }
+
         switch (parsedURL.pathname) {
             case "/":
                 serveIndex(req, res, "index.html");
                 break;
             case "/static/main.css":
                 serveStatic(req, res, "main.css");
-                break;
-            case "/product":
-                serveStatic(req, res, "product.html");
-                break;
-            case "/product1":
-                serveStatic(req, res, "product1.html");
                 break;
             case "/static/product.css":
                 serveStatic(req, res, "product.css");
@@ -46,10 +45,26 @@ function serveStatic(req, res, customFileName) {
 
 function serveIndex(req, res, customFileName) {
     const filename = setHeaderForFile(req, res, customFileName);
-    const template = ejs.compile(fs.readFileSync('static/' + filename).toString());
+    const template = ejs.compile(fs.readFileSync(filename).toString());
     const products = ProductService.getProducts();
     const scope = {
         products: products
+    };
+    const content = template(scope);
+    res.write(content);
+    res.end();
+}
+
+function serveProduct(req, res, customFileName) {
+    const filename = setHeaderForFile(req, res, customFileName);
+    const template = ejs.compile(fs.readFileSync(filename).toString());
+    const url = URL.parse(req.url);
+    const slugPart = url.pathname.replace("/product/", "");
+    const slugParts = slugPart.split("-");
+    const key = slugParts[0];
+    const desiredProduct = ProductService.getProductByKey(key);
+    const scope = {
+        product: desiredProduct
     };
     const content = template(scope);
     res.write(content);
