f39ac3c75ccf848f01a26217822ab4abbe9b29ca
Ubuntu <ubuntu@ip-172-31-43-17.us-east-2.compute.internal>
2020-12-13 20:52:07 +0000

    translation of jsx(babel)

--- webpack.js
+++ webpack.js
@@ -1,18 +1,39 @@
 const path = require('path');
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
     mode: "development",
     entry: "./static/app.jsx",
     output: {
+        filename: "bundle.js",
         path: path.resolve(__dirname, "public")
     },
 
     plugins: [
-        new CleanWebpackPlugin(["public"]),
-        new CopyWebpackPlugin([{
-            from: "static",
-            to: ""
-        }]),
-    ]
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [{
+                from: "static",
+                to: "",
+                globOptions: {
+                    ignore: [
+                        "*.jsx"
+                    ]
+                }
+            }]
+        })
+    ],
+
+    module: {
+        rules: [{
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["babel-preset-react"]
+                }
+            }
+        }]
+    },
 };
