df6114faabcadfe9ac50438195cc104f6cbb0eb0
Ubuntu <ubuntu@ip-172-31-43-17.us-east-2.compute.internal>
2020-12-29 15:19:06 +0000

    fetch data from db

--- public/pages/IndexPage.jsx
+++ public/pages/IndexPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+const fetch = require("node-fetch");
 export default class IndexPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: []
+        };
+    }
+
     render() {
         return <React.Fragment>
 <header className="row bg-primary">
@@ -18,29 +25,7 @@ export default class IndexPage extends React.Component {
 </header>
 
 <main className="row">
-    <div className="col-sm-5 offset-sm-1">
-            <div class="card">
-                <img className="card-img-top" src="static/product.png" alt="Card image cap"/>
-                <div className="card-body">
-                    <h5 className="card-title">ПВУ Turkov ZENIT 350 HECO</h5>
-                    <p  className="card-text">Вентиляционная установка с рекуперацией тепла и влаги</p>
-                    <h5  className="card-title">Цена: 5000 грн.</h5>
-                    <Link to="/product/prod1" className="btn btn-primary ">Заказать</Link>
-                </div>
-            </div>
-    </div>
-
-    <div  className="col-sm-5 offset-sm-1">
-            <div  className="card">
-                <img  className="card-img-top " src="static/product1.png" alt="Card image cap"/>
-                <div  className="card-body">
-                    <h5  className="card-title">Beats Studio Wireless Over-Ear Headphones</h5>
-                    <p  className="card-text">Беспроводные наушники Beats Studio</p>
-                    <h5  className="card-title">Цена: 10000 грн.</h5>
-                    <Link to="/product/prod2" className="btn btn-primary ">Заказать</Link>
-                </div>
-            </div>
-    </div>
+     {this.state.products? this.renderProducts() : false} 
 </main>
 
 <footer className="row">
@@ -49,5 +34,31 @@ export default class IndexPage extends React.Component {
     </div>
 </footer>
 </React.Fragment>;
+    }
+
+    componentDidMount() {
+        fetch("api/products").then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            this.setState({
+                products: json
+            });
+        }.bind(this));
+    }
+
+    renderProducts() {
+        return this.state.products.map(function(item, index) {
+            return <div className ="col-sm-5 offset-sm-1">
+            <div class="card">
+                <img className="card-img-top" src={item.imgURL} alt="Card image cap"/>
+                <div className="card-body">
+                    <h5 className="card-title">{item.title}</h5>
+                    <p  className="card-text">{item.description}</p>
+                    <h5  className="card-title">Цена: {item.price} грн.</h5>
+                    <Link to={"/product/prod"+ (index+1)} className="btn btn-primary ">Заказать</Link>
+                </div>
+            </div> 
+            </div>;
+        }, this);
     }
 }
